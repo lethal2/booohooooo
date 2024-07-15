473cc430899507de3efc2312ab133e924d00a86a
Hackerman694200 <158832966+Hackerman694200@users.noreply.github.com>
2024-07-15 06:39:33 -0400

    Update codetemp.lua

--- codetemp.lua
+++ codetemp.lua
@@ -3367,25 +3367,47 @@ do
 
 			while true do		
 				pcall(function()
-					local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
-					local Server, Next = nil, nil
+local gameId = 606849621
 
-					local function ListServers(cursor)
-						local Raw = game:HttpGet(Servers .. ((cursor and "&cursor="..cursor) or ""))
+local function get(url)
+    return game:HttpGet(url, true)
+end
 
-						return HttpService:JSONDecode(Raw)
-					end
+local function findSmallestServer()
+    local servers = {}
+    local nextPageCursor = ""
+    repeat
+        local url = "https://games.roblox.com/v1/games/" .. gameId .. "/servers/Public?sortOrder=Asc&limit=100"
+        if nextPageCursor ~= "" then
+            url = url .. "&cursor=" .. nextPageCursor
+        end
+        
+        local data = game:GetService("HttpService"):JSONDecode(get(url))
+        
+        for _, server in ipairs(data.data) do
+            if server.playing < server.maxPlayers then
+                table.insert(servers, server)
+            end
+        end
+        
+        nextPageCursor = data.nextPageCursor
+    until not nextPageCursor
+    
+    if #servers > 0 then
+        table.sort(servers, function(a, b) return a.playing < b.playing end)
+        return servers[1].id
+    end
+    
+    return nil
+end
 
-					repeat
-						local Servers = ListServers(Next)
-						Server = Servers.data[math.random(1, (#Servers.data / 3))]
-						Next = Servers.nextPageCursor
-					until Server
-
-					if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
-						TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, player)
-					end
-
+local serverId = findSmallestServer()
+if serverId then
+    game:GetService("TeleportService"):TeleportToPlaceInstance(gameId, serverId)
+else
+    
+end
+							
 					task.wait(10)
 				end)
 			end
